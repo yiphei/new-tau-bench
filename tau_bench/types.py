426bc7596c9edd120f6543f3468173e9cbd85a53
Yifei Yan <yanyiphei@gmail.com>
2025-04-06 15:00:20 -0700

    added changes

--- tau_bench/types.py
+++ tau_bench/types.py
@@ -74,9 +74,16 @@ class EnvResetResponse(BaseModel):
 class EnvRunResult(BaseModel):
     task_id: int
     reward: float
+    write_actions_diff: Optional[Dict[str, Any]] = None
+    actions_diff: Optional[Dict[str, Any]] = None
     info: Dict[str, Any]
+    key_actions: Optional[List[Action]] = None
     traj: List[Dict[str, Any]]
     trial: int
+    raw_messages: Optional[List[Dict[str, Any]]] = None
+    node_turns: Optional[List[Dict]] = None
+    oai_messages: Optional[List[Dict[str, Any]]] = None
+    anthropic_messages: Optional[List[Dict[str, Any]]] = None
 
 
 class RunConfig(BaseModel):
